2eac9e12680a0f03a70171d76a0fb6ed5355ce8f
Adillah Putri <adllhputri@gmail.com>
2024-05-07 18:21:35 +0700

    Add callback

--- index.mjs
+++ index.mjs
@@ -166,9 +166,9 @@ io.on("connection", (socket) => {
         callback(true)
       })
 
-      socket.on("sendToPrivate", (data) => {
+      socket.on("sendToPrivate", (data, callback) => {
         sendToPeer(data.to, data.channel, data.message)
-        return true;
+        callback(true)
       })
     }
   })
